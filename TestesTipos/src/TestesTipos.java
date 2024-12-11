5857ab468ce5aff6518e31b880dc79b046c75912
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-12-11 09:09:18 -0300

    testes tipos

---
+++ TestesTipos/src/TestesTipos.java
@@ -0,0 +1,17 @@
+
+
+/**
+ *
+ * @author Rebecca
+ */
+public class TestesTipos {
+
+    public static void main(String[] args) {
+        /*int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.print(valor);*/
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.print(idade);
+    }
+}
