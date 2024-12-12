37ebd2bcdd9f2b4c22dd024300f548690a6b285d
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-12-12 09:48:27 -0300

    correção

--- TiposPrimitivos/src/TiposPrimitivos.java
+++ TiposPrimitivos/src/TiposPrimitivos.java
@@ -7,12 +7,13 @@ import java.util.Scanner;
 public class TiposPrimitivos {
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Digite o nome do aluno: ");
-        String nome = teclado.nextLine();
-        System.out.print("Digite a nota do aluno: ");
-        float nota = teclado.nextFloat();
-        System.out.format("A nota de %s é %.2f.", nome, nota);
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.print("Digite o nome do aluno: ");
+            String nome = teclado.nextLine();
+            System.out.print("Digite a nota do aluno: ");
+            float nota = teclado.nextFloat();
+            System.out.format("A nota de %s é %.2f.", nome, nota);
+        }
 
 
     }
