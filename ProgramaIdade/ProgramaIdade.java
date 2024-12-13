4f15f03531ec7633148387bd4803a08b16e33701
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-12-13 09:27:07 -0300

    verificando maioridade

---
+++ ProgramaIdade/ProgramaIdade.java
@@ -0,0 +1,33 @@
+package ProgramaIdade;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+import java.util.Scanner;
+/**
+ *
+ * @author Rebecca
+ */
+public class ProgramaIdade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Scanner t = new Scanner(System.in);
+        System.out.print("Em que ano voce nasceu? ");
+        int nasc = t.nextInt();
+        int i = 2024-nasc;
+        System.out.print("Sua idade é " + i);
+        if (i>= 18){
+            System.out.print(" anos, e voce é maior de idade.");
+        } else {
+            System.out.println("anos, e você é menor de idade.");
+        } 
+    }
+    
+}
